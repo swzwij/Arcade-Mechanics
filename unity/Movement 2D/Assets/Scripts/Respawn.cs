c81668927cd35785a9f37a3ab392040973cf5005
Joost-Robat <30769@ma-web.nl>
2021-04-19 13:25:22 +0200

    Animation fixed and work properly now, also tried a few things with respawning script but didn't trun out well so reverted to the old one.

--- unity/Movement 2D/Assets/Scripts/Respawn.cs
+++ unity/Movement 2D/Assets/Scripts/Respawn.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
+    public bool timerOn = false;
+    public float time = 5f;
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        
         player.transform.position = respawnPoint.transform.position;
     }
 }
